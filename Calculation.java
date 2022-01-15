26e0528f4077968b24ed7c4f6bf55b66df62a70e
Dharshini <dharshiniytoo@gmail.com>
2022-01-15 16:59:12 +0530

    my first java program

---
+++ Calculation.java
@@ -0,0 +1,19 @@
+class Calculation{
+public static void main(String args[]){
+Add addition =new Add();
+addition.a=10;
+addition.b=20;
+addition.add();
+
+Sub subtraction =new Sub();
+subtraction.a=55;
+subtraction.b=25;
+subtraction.sub();
+
+Divi dharshini =new Divi();
+dharshini.a=100;
+dharshini.b=23;
+dharshini.divi();
+
+}
+}
